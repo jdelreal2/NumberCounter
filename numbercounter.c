040ed3a8826cdac97c1414adce3567a25582da17
jdelreal2 <82716714+jdelreal2@users.noreply.github.com>
2021-04-17 10:52:39 -0400

    Create numbercounter.c

---
+++ numbercounter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    printf ("Count from 1 to 1000\n");
+    int i;
+    for (i=0; i<=1000; i++) {
+        printf ("Currently: '%d'.\n", i);
+        // foo
+    }
+}
